596c9d4856fa9e1643073bf81ed6e65699059f98
baizid20 <baizidul20@gmail.com>
2025-04-07 14:47:31 +0600

    Add money section complete successfully

---
+++ javaScript/addMoney.js
@@ -0,0 +1,48 @@
+document.getElementById('add-money').addEventListener('click',function(){
+    this.classList.add('bg-green-300')
+     document.getElementById('latest-payment').classList.add('hidden')
+     document.getElementById('add-mony-form').classList.remove('hidden')
+   
+    
+    
+})
+
+document.getElementById('add-Money-login').addEventListener('click',function(event){
+    event.preventDefault();
+    let moneyAdd = document.getElementById('add-amount').value 
+    let convertMoneyAdd = parseFloat(moneyAdd)
+   
+    let mainMoney = document.getElementById('money').innerText
+    let convertMainMoney = parseFloat(mainMoney)
+
+    const pin = document.getElementById('pin').value
+    const accountNum = document.getElementById('account-num').value
+
+
+    if(pin.length === 4 && accountNum.length === 11){
+         
+        let result = convertMainMoney + convertMoneyAdd;
+        document.getElementById('money').innerText = result;
+    }
+
+   
+
+
+  
+
+
+    // console.log(typeof(money))
+    
+    // let total = parseFloat('moneyAdd') + parseFloat('mainMoney')
+    // let result = document.getElementById('money').innerText = total
+
+//     let convertMoneyAdd = parseFloat('moneyAdd')
+
+//    
+//     let convertmainMoney = parseFloat(mainMoney)
+//    let totalMoney = convertMoneyAdd + convertmainMoney
+//    let totalMoneyConvert = parseFloat('totalMoney')
+      
+//    document.getElementById('money').innerText = totalMoneyConvert
+
+})
